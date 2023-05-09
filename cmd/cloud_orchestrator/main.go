fafc2bf45503cd2b59f0319e08531e05a3f2c0d6
Jorge E. Moreira <jemoreira@google.com>
2023-05-09 14:35:57 -0700

    Reorganize code in the cloud_orchestrator main file

--- cmd/cloud_orchestrator/main.go
+++ cmd/cloud_orchestrator/main.go
@@ -30,7 +30,7 @@ import (
 	"google.golang.org/api/compute/v1"
 )
 
-func main() {
+func LoadConfiguration() *app.Config {
 	config, err := app.LoadConfig()
 	if err != nil {
 		log.Fatal("Failed to load configuration: ", err)
@@ -40,7 +40,10 @@ func main() {
 	if config.InstanceManager.Type == app.GCEIMType {
 		log.Println("  GCP Project: " + config.InstanceManager.GCP.ProjectID)
 	}
+	return config
+}
 
+func LoadInstanceManager(config *app.Config) app.InstanceManager {
 	var im app.InstanceManager
 	switch config.InstanceManager.Type {
 	case app.GCEIMType:
@@ -57,12 +60,10 @@ func main() {
 	default:
 		log.Fatal("Unknown Instance Manager type: ", config.InstanceManager.Type)
 	}
+	return im
+}
 
-	ss := app.NewForwardingSignalingServer(config.WebStaticFilesPath, im)
-
-	// The network interface to listen on. Empty means all interfaces, which the right choice in production
-	iface := ""
-
+func LoadSecretManager(config *app.Config) app.SecretManager {
 	var sm app.SecretManager
 	switch config.SecretManager.Type {
 	case app.GCPSMType:
@@ -80,7 +81,10 @@ func main() {
 	default:
 		log.Fatal("Unknown Secret Manager type: ", config.SecretManager.Type)
 	}
+	return sm
+}
 
+func LoadOAuthConfig(config *app.Config, sm app.SecretManager) *oauth2.Config {
 	var oauthConfig *oauth2.Config
 	switch config.AccountManager.OAuth.Provider {
 	case app.GoogleOAuthProvider:
@@ -88,7 +92,10 @@ func main() {
 	default:
 		log.Fatal("Unknown oauth provider: ", app.GoogleOAuthProvider)
 	}
+	return oauthConfig
+}
 
+func LoadAccountManager(config *app.Config, oauthConfig *oauth2.Config) app.AccountManager {
 	var am app.AccountManager
 	switch config.AccountManager.Type {
 	case app.GAEAMType:
@@ -97,22 +104,46 @@ func main() {
 		am = &unix.AccountManager{
 			OAuthConfig: oauthConfig,
 		}
-		// This account manager is insecure, it's only meant for development. It's generally not
-		// safe to listen on every interface when it's in use, so restrict it to the loopback
-		// interface only.
-		iface = "localhost"
 	default:
 		log.Fatal("Unknown Account Manager type: ", config.AccountManager.Type)
 	}
+	return am
+}
 
-	or := app.NewController(config.Infra.STUNServers, config.Operations, im, ss, am)
+// The network interface for the web server to listen on.
+func ChooseNetworkInterface(config *app.Config) string {
+	if config.AccountManager.Type == app.UnixAMType {
+		// This account manager is insecure, it's only meant for development. It's generally not
+		// safe to listen on every interface when it's in use, so restrict it to the loopback
+		// interface only.
+		return "localhost"
+	}
+	// Empty means all interfaces, which is the right choice in production.
+	return ""
+}
 
+func ServerPort() string {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 		log.Printf("Defaulting to port %s", port)
 	}
+	return port
+}
+
+func main() {
+	config := LoadConfiguration()
+
+	instanceManager := LoadInstanceManager(config)
+	signalingServer := app.NewForwardingSignalingServer(config.WebStaticFilesPath, instanceManager)
+	secretManager := LoadSecretManager(config)
+	oauthConfig := LoadOAuthConfig(config, secretManager)
+	accountManager := LoadAccountManager(config, oauthConfig)
+	controller := app.NewController(config.Infra.STUNServers, config.Operations, instanceManager, signalingServer, accountManager)
+
+	iface := ChooseNetworkInterface(config)
+	port := ServerPort()
 
 	log.Printf("Listening on port %s", port)
-	log.Fatal(http.ListenAndServe(iface+":"+port, or.Handler()))
+	log.Fatal(http.ListenAndServe(iface+":"+port, controller.Handler()))
 }

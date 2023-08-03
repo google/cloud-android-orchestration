0b31e6073d2137750b099c639213f230168edcee
sangachoi <sangachoi@google.com>
2023-08-03 01:50:31 +0000

    Add unregister feature

--- web/page0/src/app/runtime-card/runtime-card.component.ts
+++ web/page0/src/app/runtime-card/runtime-card.component.ts
@@ -1,24 +1,31 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Runtime } from '../runtime-interface';
+import { RuntimeService } from '../runtime.service';
 
 @Component({
   selector: 'app-runtime-card',
   templateUrl: './runtime-card.component.html',
-  styleUrls: ['./runtime-card.component.scss']
+  styleUrls: ['./runtime-card.component.scss'],
 })
 export class RuntimeCardComponent {
   @Input() runtime: Runtime | null = null;
-  panelOpenState = false
+  panelOpenState = false;
 
-  status = "error"
+  status = 'error';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private runtimeService: RuntimeService) {}
   onClickAddHost() {
-    this.router.navigate(
-      ['/create-host'],
-      { queryParams: { runtime: this.runtime?.alias }}
-    )
+    this.router.navigate(['/create-host'], {
+      queryParams: { runtime: this.runtime?.alias },
+    });
+  }
 
+  onClickDelete(alias: string | undefined) {
+    if (!alias) {
+      return;
+    }
+
+    this.runtimeService.unregisterRuntime(alias);
   }
 }

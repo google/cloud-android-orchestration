6aa268a67260898539837b16f4c875ddc1ec2b54
sangachoi <sangachoi@google.com>
2023-08-03 11:48:36 +0000

    Add status to runtime card

--- web/page0/src/app/runtime-interface.ts
+++ web/page0/src/app/runtime-interface.ts
@@ -2,12 +2,11 @@ export interface Runtime {
   alias: string;
   runtime_type: 'local' | 'on-premise' | 'cloud';
   url: string;
-  hosts: string[];
+  hosts?: string[]; // Full urls towards host orchestrators
   status: 'valid' | 'error' | 'loading';
 }
 
 export interface RuntimeAdditionalInfo {
   runtime_type: 'local' | 'on-premise' | 'cloud';
   hosts: string[];
-  status: 'valid' | 'error' | 'loading';
 }

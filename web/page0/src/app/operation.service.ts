83fa2e2b2bdbd02406b8184f28c342c8099640c0
retroinspect <sangachoi@google.com>
2023-08-31 01:51:32 +0000

    Seperate retryConfig from OperationService

--- web/page0/src/app/operation.service.ts
+++ web/page0/src/app/operation.service.ts
@@ -1,6 +1,6 @@
 import {HttpErrorResponse} from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import {Observable, of, throwError} from 'rxjs';
+import {Observable, timer} from 'rxjs';
 import {retry} from 'rxjs/operators';
 import {ApiService} from './api.service';
 
@@ -11,25 +11,23 @@ export class OperationService {
   constructor(private apiService: ApiService) {}
 
   longPolling<T>(waitUrl: string): Observable<T> {
-    return this.apiService.wait<T>(waitUrl).pipe(
-      retry({
-        count: 1000,
-        delay: (err: HttpErrorResponse, retryCount) => {
-          if (retryCount % 10 === 0) {
-            console.warn(
-              `Wait toward ${waitUrl} has failed for ${retryCount} times`
-            );
-          }
-
-          if (err.status === 503) {
-            return of(1);
-          }
-
-          return throwError(
-            () => new Error(`Request ${err.url} failed to be done`)
+    const retryConfig = {
+      count: 1000,
+      delay: (err: HttpErrorResponse, retryCount: number) => {
+        if (retryCount % 10 === 0) {
+          console.warn(
+            `Wait toward ${waitUrl} has failed for ${retryCount} times`
           );
-        },
-      })
-    );
+        }
+
+        if (err.status === 503) {
+          return timer(0);
+        }
+
+        throw new Error(`Request ${err.url} failed to be done`);
+      },
+    };
+
+    return this.apiService.wait<T>(waitUrl).pipe(retry(retryConfig));
   }
 }

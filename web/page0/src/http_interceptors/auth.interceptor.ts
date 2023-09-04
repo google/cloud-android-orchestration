9dedef45a00e6c5badc1b7d84fab2a8393de49be
retroinspect <sangachoi@google.com>
2023-09-04 08:11:16 +0000

    Add placeholder interceptor to catch auth failure

---
+++ web/page0/src/http_interceptors/auth.interceptor.ts
@@ -0,0 +1,38 @@
+import {Injectable} from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+const handleAuthError = (error: HttpErrorResponse, snackBar: MatSnackBar) => {
+  console.error(error);
+  snackBar.open('Request failed: check your credentials');
+};
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private snackBar: MatSnackBar) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        // If credentails got expired, OPTIONS request fails before the actual request
+        // Thus, error status becomes 0 for CORS failure
+        if (error.status === 0) {
+          handleAuthError(error, this.snackBar);
+        }
+
+        throw error;
+      })
+    );
+  }
+}

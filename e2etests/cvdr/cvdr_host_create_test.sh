1149913545e77ccc9e0093660c1df01da5a4492d
Seungjae Yoo <seungjaeyoo@google.com>
2025-09-29 16:15:03 +0900

    cvdr host create E2E check multiple host creation

--- e2etests/cvdr/cvdr_host_create_test.sh
+++ e2etests/cvdr/cvdr_host_create_test.sh
@@ -4,5 +4,14 @@ set -e -x
 source ${TEST_SRCDIR}/_main/cvdr/common_utils.sh
 validate_components
 
-HOSTNAME=$(cvdr host create)
-cvdr host delete ${HOSTNAME}
+cleanup() {
+    cvdr host delete ${HOSTNAME[@]}
+}
+trap cleanup EXIT ERR
+for i in $(seq 0 1); do
+    HOSTNAME[i]=$(cvdr host create)
+    if [[ ! $(cvdr host list) =~ ${HOSTNAME[i]} ]]; then
+        echo "Host ${HOSTNAME[i]} doesn't exist"
+        exit 1
+    fi
+done

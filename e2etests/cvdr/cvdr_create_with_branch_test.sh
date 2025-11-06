5d63f824c68050450e414c631d9bd4c2ab2360c1
Seungjae Yoo <seungjaeyoo@google.com>
2025-11-06 15:09:51 +0900

    Use cvdr get_config for retrieving service URL from E2E test

--- e2etests/cvdr/cvdr_create_with_branch_test.sh
+++ e2etests/cvdr/cvdr_create_with_branch_test.sh
@@ -4,6 +4,9 @@ set -e -x
 source ${TEST_SRCDIR}/_main/cvdr/common_utils.sh
 validate_components
 
+SERVICE=$(cvdr get_config user_default_service || cvdr get_config system_default_service)
+SERVICE_URL=$(cvdr get_config "services[\"$SERVICE\"].service_url")
+
 HOSTNAME=$(cvdr host create)
 cleanup() {
     cvdr host delete ${HOSTNAME}
@@ -19,13 +22,13 @@ cvdr create \
 # TODO(b/448209030): cvdr list should print proper ADB connection status.
 ACTUAL_OUTPUT=$(cvdr list --host ${HOSTNAME})
 EXPECTED_OUTPUT="Host: ${HOSTNAME}
-  WebUI: http://localhost:8080/v1/zones/local/hosts/${HOSTNAME}/
+  WebUI: $SERVICE_URL/v1/zones/local/hosts/${HOSTNAME}/
   Group: cvd
     Instance: 1
       Status: Running
       ADB: not connected
       Displays: [720 x 1280 ( 320 )]
-      Logs: http://localhost:8080/v1/zones/local/hosts/${HOSTNAME}/cvds/cvd/1/logs/"
+      Logs: $SERVICE_URL/v1/zones/local/hosts/${HOSTNAME}/cvds/cvd/1/logs/"
 diff <(echo ${EXPECTED_OUTPUT}) <(echo ${ACTUAL_OUTPUT})
 
 # Check ADB connection
--- e2etests/cvdr/cvdr_create_with_local_srcs_test.sh
+++ e2etests/cvdr/cvdr_create_with_local_srcs_test.sh
@@ -15,6 +15,9 @@ if [ ! -f "${IMAGE_ZIP}" ]; then
     exit 1
 fi
 
+SERVICE=$(cvdr get_config user_default_service || cvdr get_config system_default_service)
+SERVICE_URL=$(cvdr get_config "services[\"$SERVICE\"].service_url")
+
 HOSTNAME=$(cvdr host create)
 cleanup() {
     cvdr host delete ${HOSTNAME}
@@ -30,13 +33,13 @@ cvdr create \
 # TODO(b/448209030): cvdr list should print proper ADB connection status.
 ACTUAL_OUTPUT=$(cvdr list --host ${HOSTNAME})
 EXPECTED_OUTPUT="Host: ${HOSTNAME}
-  WebUI: http://localhost:8080/v1/zones/local/hosts/${HOSTNAME}/
+  WebUI: $SERVICE_URL/v1/zones/local/hosts/${HOSTNAME}/
   Group: cvd_1
     Instance: 1
       Status: Running
       ADB: not connected
       Displays: [720 x 1280 ( 320 )]
-      Logs: http://localhost:8080/v1/zones/local/hosts/${HOSTNAME}/cvds/cvd_1/1/logs/"
+      Logs: $SERVICE_URL/v1/zones/local/hosts/${HOSTNAME}/cvds/cvd_1/1/logs/"
 diff <(echo ${EXPECTED_OUTPUT}) <(echo ${ACTUAL_OUTPUT})
 
 # Check ADB connection
